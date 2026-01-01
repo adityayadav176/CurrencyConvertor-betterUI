133369802b9b4946e8322c5ad67b28083b2cc712
adityayadav176 <ay6598aditya@gmail.com>
2026-01-02 00:31:38 +0530

    add swap btn css

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const toggle = document.getElementById("mode");
 const body = document.body;
 const container1 = document.querySelector(".container1");
 const containers = document.querySelector(".container");
+const swapbtn = document.querySelector(".swap-btn");
+
 toggle.addEventListener("change", () => {
     body.classList.toggle("dark");
     container1.classList.toggle("color");
     containers.classList.toggle("color");
+    swapbtn.classList.toggle("color");
 });
 
 
