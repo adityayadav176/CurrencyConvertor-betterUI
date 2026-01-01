ef50783324237e7c8d992bf7bd8719d1f3461e40
adityayadav176 <ay6598aditya@gmail.com>
2026-01-01 23:06:29 +0530

    add toggle

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById("mode");
+const body = document.body;
+const container1 = document.querySelector(".container1");
+const containers = document.querySelector(".container");
+toggle.addEventListener("change", () => {
+    body.classList.toggle("dark");
+    container1.classList.toggle("color");
+    containers.classList.toggle("color");
+});
+
+
