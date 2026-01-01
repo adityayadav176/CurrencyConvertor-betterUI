f348214e8741864ada99c8dbfa6300e8ad15a0e1
adityayadav176 <ay6598aditya@gmail.com>
2026-01-01 22:57:36 +0530

    bug fixes

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ const toggle = document.getElementById("mode");
 const body = document.body;
 const container1 = document.querySelector(".container1");
 const containers = document.querySelector(".container");
-const input = document.querySelector("#EnterData");
-const select = document.querySelector("select");
-const convertorPara = document.querySelector("#convertor-para");
-const updatedPara = document.querySelector("#updated-date");
-const refreshBtn = document.querySelector("#refreshBtn");
-const swapBtn = document.querySelector(".swap-btn");
-const fromCurr = document.querySelector(".box");
-
-
 toggle.addEventListener("change", () => {
     body.classList.toggle("dark");
     container1.classList.toggle("color");
