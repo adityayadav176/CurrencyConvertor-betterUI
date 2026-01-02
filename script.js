2683256b22522c9f42455fab39fb67ed39ddbafc
adityayadav176 <ay6598aditya@gmail.com>
2026-01-02 21:50:33 +0530

    bug fixes

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+// currency-convertor API
+const base_url = "https://2024-03-06.currency-api.pages.dev/v1/currencies";
+
 const toggle = document.getElementById("mode");
 const body = document.body;
 const container1 = document.querySelector(".container1");
 const containers = document.querySelector(".container");
 const swapBtn = document.querySelector(".swap-btn");
-
-// select all dropdown
 const selects = document.querySelectorAll("select");
+const toCurr = document.querySelector(".toCurr select");
+const fromCurr = document.querySelector(".fromCurr select");
+const msg = document.querySelector("#msg");
+const updatedMsg = document.querySelector("#updated-msg");
+const randMsg = document.querySelector(".box1-para");
 
-// select all coutry
+
+// Populated dropdown
 for( let select of selects){
     for(let currCode in countryList){
         let newOption = document.createElement("option");
@@ -24,8 +31,7 @@ for( let select of selects){
         select.append(newOption);
     }
 }
-
-
+// toggle btn
 toggle.addEventListener("change", () => {
     body.classList.toggle("dark");
     container1.classList.toggle("color");
