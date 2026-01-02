ba5b0275432c717847192717ac8c5b1d729c354d
adityayadav176 <ay6598aditya@gmail.com>
2026-01-02 23:30:09 +0530

    ready currencyConvertor app

--- script.js
+++ script.js
@@ -6,12 +6,67 @@ const body = document.body;
 const container1 = document.querySelector(".container1");
 const containers = document.querySelector(".container");
 const swapBtn = document.querySelector(".swap-btn");
+
+
+const amountInput = document.querySelector("#fromCurr"); // input
+const resultText = document.querySelector("#toCurr");    // result <p>
+
 const selects = document.querySelectorAll("select");
-const toCurr = document.querySelector(".toCurr select");
-const fromCurr = document.querySelector(".fromCurr select");
+const fromCurr = selects[0]; // first select
+const toCurr = selects[1];   // second select
+
 const msg = document.querySelector("#msg");
 const updatedMsg = document.querySelector("#updated-msg");
 const randMsg = document.querySelector(".box1-para");
+
+// API called
+
+const getExchangeRate = async () => {
+    let amount = amountInput.value;
+
+    if (amount === "" || amount <= 0) {
+        amount = 1;
+        amountInput.value = 1;
+    }
+
+    const from = fromCurr.value.toLowerCase();
+    const to = toCurr.value.toLowerCase();
+
+    try {
+        const URL = `${base_url}/${from}.json`;
+        const response = await fetch(URL);
+        const data = await response.json();
+
+        const rate = data[from][to];
+        const finalAmount = (amount * rate).toFixed(2);
+
+        // Update UI
+        resultText.innerText = finalAmount;
+        msg.innerText = `1 ${from.toUpperCase()} = ${rate} ${to.toUpperCase()}`;
+        updatedMsg.innerText = `Last Updated : ${new Date().toLocaleTimeString()}`;
+
+        updateQuickCards(rate);
+
+    } catch (error) {
+        console.error(error);
+        msg.innerText = "Failed to load exchange rate";
+    }
+};
+// convert cards
+function updateQuickCards(rate) {
+    const cards = document.querySelectorAll(".container");
+    const amounts = [100, 500, 1000];
+
+    cards.forEach((card, index) => {
+        card.querySelector(".currency-rate").innerText = amounts[index];
+        card.querySelector(".currency-name").innerText = fromCurr.value;
+
+        card.querySelector(".bottomCurrRate").innerText =
+            (amounts[index] * rate).toFixed(0);
+
+        card.querySelector(".bottomCurrName").innerText = toCurr.value;
+    });
+}
 
 
 // Populated dropdown
@@ -38,5 +93,28 @@ toggle.addEventListener("change", () => {
     containers.classList.toggle("color");
     swapBtn.classList.toggle("color");
 });
+// swapBtn
+swapBtn.addEventListener("click", () => {
+    const temp = fromCurr.value;
+    fromCurr.value = toCurr.value;
+    toCurr.value = temp;
+
+    getExchangeRate();
+});
+//events
+amountInput.addEventListener("input", getExchangeRate);
+fromCurr.addEventListener("change", getExchangeRate);
+toCurr.addEventListener("change", getExchangeRate);
+
+document.querySelector("#refreshBtn").addEventListener("click", getExchangeRate);
+
+//intial load
+
+window.addEventListener("load", () => {
+    amountInput.value = 1;
+    getExchangeRate();
+});
+
+
 
 
