06f70d7c9eb0360ec184ac3fa765c99f08d2c78a
adityayadav176 <ay6598aditya@gmail.com>
2026-01-02 20:26:33 +0530

    bug fixes

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const body = document.body;
 const container1 = document.querySelector(".container1");
 const containers = document.querySelector(".container");
 const swapBtn = document.querySelector(".swap-btn");
-const box1UpperText = document.querySelector
+
 // select all dropdown
 const selects = document.querySelectorAll("select");
 
-// select all country
+// select all coutry
 for( let select of selects){
     for(let currCode in countryList){
         let newOption = document.createElement("option");
@@ -24,10 +24,6 @@ for( let select of selects){
         select.append(newOption);
     }
 }
-
-//populate text bottom in box1
-
-
 
 
 toggle.addEventListener("change", () => {
