6d99c69c76b74fa6acddaee583023e31ad09299c
adityayadav176 <ay6598aditya@gmail.com>
2026-01-02 19:47:44 +0530

    add dropdowns

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ const toggle = document.getElementById("mode");
 const body = document.body;
 const container1 = document.querySelector(".container1");
 const containers = document.querySelector(".container");
-const swapBtn = document.querySelector(".swap-btn")
+const swapBtn = document.querySelector(".swap-btn");
+
+// select all dropdown
+const selects = document.querySelectorAll("select");
 
 // select all coutry
+for( let select of selects){
+    for(let currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+
+        if(select.name === "from" && currCode === "USD"){
+            newOption.selected = true;
+        }
+        if(select.name === "to" && currCode === "INR"){
+            newOption.selected = true;
+        }
+
+        select.append(newOption);
+    }
+}
+
 
 toggle.addEventListener("change", () => {
     body.classList.toggle("dark");
