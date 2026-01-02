84ee2a025c8b86558d5c210a432212eb0cbe851b
adityayadav176 <ay6598aditya@gmail.com>
2026-01-02 11:59:22 +0530

    add new file

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const toggle = document.getElementById("mode");
 const body = document.body;
 const container1 = document.querySelector(".container1");
 const containers = document.querySelector(".container");
-const swapbtn = document.querySelector(".swap-btn");
+
+// select all coutry
 
 toggle.addEventListener("change", () => {
     body.classList.toggle("dark");
     container1.classList.toggle("color");
     containers.classList.toggle("color");
-    swapbtn.classList.toggle("color");
+    swapBtn.classList.toggle("color");
+
 });
 
 
